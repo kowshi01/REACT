54bc5a27ae4eeeb2513d14dc2443e696a6d6582d
Kowshika K <36320336+kowshi01@users.noreply.github.com>
2023-09-12 10:41:42 +0530

    Styling and using JS in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
-
+import './ExpenseItem.css'
 function ExpenseItem() {
+    const expenseDate= new Date(2023,6,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=200;
+    const LocationOfExpenditure='Amount spent location';
   return  (
-  <div>
-    <h1>Expense Items</h1>
-    <p>Food Rs 10</p>
-    <p>Petrol Rs 100</p>
-    <p>Movies Rs 200</p>
+  <div className="expense-item">
+    <div>{expenseDate.toISOString()}</div>
+    <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h3>{LocationOfExpenditure}</h3>
+        <div className="expense-item__price">${expenseAmount}</div>
+    </div>
   </div>
   )
 }
