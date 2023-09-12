ad766de700cee2a13d74980e5da7c0b00cda9473
Kowshika K <36320336+kowshi01@users.noreply.github.com>
2023-09-12 11:33:33 +0530

    Passing Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const expenseDate= new Date(2023,6,28);
-    const expenseTitle='Car Insurance';
-    const expenseAmount=200;
-    const LocationOfExpenditure='Amount spent location';
+function ExpenseItem(props) {
+    
   return  (
   <div className="expense-item">
-    <div>{expenseDate.toISOString()}</div>
+    <div>{props.date.toISOString()}</div>
     <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <h3>{LocationOfExpenditure}</h3>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <h3>{props.location}</h3>
+        <div className="expense-item__price">${props.amount}</div>
     </div>
   </div>
   )
