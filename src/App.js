1bff0b0d07a44cc1fd9cdfbde52f39028da380e2
Kowshika K <36320336+kowshi01@users.noreply.github.com>
2023-11-28 14:01:51 +0530

    Refs and Controlled Components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-import Card from './components/UI/Card';
+import React, { useState,Fragment } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
 function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const addUserHandler = (uName, uAge, uCollege) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, college:uCollege, id: Math.random().toString() },
+      ];
+    });
+  };
 
-  const LocationOfExpenditure='Amount spent location';
   return (
-    <Card>
-      <h2>Let's get started!</h2>
-      {expenses.map((expenseData)=>(
-        <ExpenseItem title={expenseData.title} amount={expenseData.amount} date={expenseData.date} location={LocationOfExpenditure}/>
-      ))}
-      
-    </Card>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
   );
 }
 
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import './Card.css';
+import React from 'react';
 
- const Card = (props) => {
-    const classes='card'+ props.className;
-   return (
-     <div className={classes}>{props.children}</div>
-   )
- }
- 
- export default Card
+import classes from './Card.module.css';
+
+const Card = (props) => {
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
+};
+
+export default Card;
